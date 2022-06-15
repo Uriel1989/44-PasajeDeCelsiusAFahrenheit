608d534d7c255d55849885916a0bae6d2d585145
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:55:15 -0300

    Add files via upload

---
+++ 44-PasajeDeCelsiusAFahrenheit/44-PasajeDeCelsiusAFahrenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _44_PasajeDeCelsiusAFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celcius, Fahrenheit;
+
+            Console.WriteLine("Ingrese un valor, será tomado como grados Celcius y pasado a Fahrenheit:");
+            Celcius = Convert.ToInt32(Console.ReadLine());
+
+            Fahrenheit = (Celcius * 1.8) + 32;
+
+            Console.WriteLine(Celcius + " grados Celcius son " + Fahrenheit + " grados Fahrenheit");
+
+            Console.ReadKey();
+        }
+    }
+}
